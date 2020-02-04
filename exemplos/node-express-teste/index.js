e70c4f5efa27bfeb161be2650144e5e510c470d1
Alvaro Ian <alvaro-ian@hotmail.com>
2020-02-04 10:18:06 -0300

    adiconar app express

---
+++ exemplos/node-express-teste/index.js
@@ -0,0 +1,23 @@
+const http = require('http')
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+
+const porta = 3000
+
+const app = express()
+app.use(morgan('dev'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res) => {
+    console.log('cabeçalhos:', req.headers)
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<h1>Hello express app</h1>')
+})
+
+const servidor = http.createServer(app)
+servidor.listen(porta, () => {
+    console.log(`servidor escutando em http://localhost:${porta}/`)
+})
