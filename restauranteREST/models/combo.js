81b10b5c3e225c8ae9548f27c1d17cf0b928d15e
alvaro-ian <alvaro-ian@hotmail.com>
2020-02-05 21:20:43 -0300

    adicionar model de combos: atividade 2

--- restauranteREST/models/combo.js
+++ restauranteREST/models/combo.js
@@ -2,5 +2,28 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const comboSchema = new Schema({
-
+    name: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    tags: [String],
+    pizzas: {
+        type: [String],
+        require: true
+    },
+    discount: {
+        type: Number,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    }
+}, {
+    timestamps: true
 })
+
+const Combo = mongoose.model('Combo', comboSchema)
+
+module.exports = Combo
