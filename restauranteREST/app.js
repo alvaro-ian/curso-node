9a48acd81efec057be74432162f5466bffec7b81
Alvaro Ian <alvaro-ian@hotmail.com>
2020-02-06 09:31:21 -0300

    adicionar autenticacao com cookies

--- restauranteREST/app.js
+++ restauranteREST/app.js
@@ -7,21 +7,47 @@ connection.then((db) => {
   console.log('conectado ao mongodb')
 }).catch(console.log)
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var comidasRouter = require('./routes/rotasComida');
-var promosRouter = require('./routes/rotasPromo');
-var combosRouter = require('./routes/rotasCombo');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const comidasRouter = require('./routes/rotasComida');
+const promosRouter = require('./routes/rotasPromo');
+const combosRouter = require('./routes/rotasCombo');
 // Exemplo
-var pizzasRouter = require('./routes/rotasPizza');
+const pizzasRouter = require('./routes/rotasPizza');
 
-var app = express();
+function auth(req, res, next) {
+  function unauthorized() {
+    const err = new Error('Não autorizado');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+  if (req.signedCookies.user) {
+    if (req.signedCookies.user != 'admin') unauthorized();
+    return next();
+  }
+
+  const header = req.headers.authorization;
+
+  if (!header) return unauthorized();
+
+  const credentials = Buffer.from(header.split(' ')[1], 'base64');
+  const [username, password] = credentials.toString().split(':');
+
+  if (username != 'admin' || password != 'admin') return unauthorized();
+
+  res.cookie('user', 'admin', { signed: true });
+  next();
+}
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,11 +56,14 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('secret'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(auth);
+
 app.use('/comidas', comidasRouter);
 app.use('/promos', promosRouter);
 app.use('/combos', combosRouter);
